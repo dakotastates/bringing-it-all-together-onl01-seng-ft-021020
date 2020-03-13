d7b4ec045d56013495029efc88f1508083a71d2b
Dakota States <dakota.states@gmail.com>
2020-03-13 23:01:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,14 +27,14 @@ class Dog
   end
   
   def save
-    if Dog.id
-      Dog.update
+    if self.id
+      self.update
     else
     sql = <<-SQL 
       INSERT INTO dogs (name, breed)
       VALUES (?, ?)
       SQL
-      DB[:conn].execute(sql, Dog.name, Dog.breed)
+      DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end
   end
