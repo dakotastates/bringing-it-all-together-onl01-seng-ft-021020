630c252e4ca94be9505288510d3245e72ee3dcb1
Dakota States <dakota.states@gmail.com>
2020-03-13 22:56:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -45,6 +45,15 @@ class Dog
     dog
   end
   
+  def self.find_by_id(id)
+    sql = <<-SQL 
+    SELECT * FROM dogs WHERE id = ?
+    SQL
+
+    result = DB[:conn].execute(sql, id)[0]
+    Dog.new(result[0], result[1], result[2])
+  end
+  
   def update
     sql = <<-SQL 
     UPDATE dogs SET name = ?, breed = ? WHERE id = ?
